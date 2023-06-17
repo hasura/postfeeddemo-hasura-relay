4922cf555d41620f84055c1bae8578380b74f615
Akshaya Acharya <akshaya.acharya.01@gmail.com>
2023-06-18 02:58:20 +0530

    Starred mutation on primary key

--- app/src/components/Star.js
+++ app/src/components/Star.js
@@ -1,16 +1,39 @@
 import graphql from "babel-plugin-relay/macro";
-import { useFragment } from "react-relay";
+import { useFragment, useMutation } from "react-relay";
 
 const StarFragment = graphql`
   fragment StarFragment on posts {
+    id
     starred
+  }
+`;
+
+const StarMutation = graphql`
+  mutation StarMutation($postId: ID!, $starred: Boolean!) {
+    update_posts_by_pk(
+      pk_columns: { id: $postId }
+      _set: { starred: $starred }
+    ) {
+      ...StarFragment
+    }
   }
 `;
 
 function Star({ post }) {
   const data = useFragment(StarFragment, post);
+
+  const [commitMutation, isMutationInFlight] = useMutation(StarMutation);
+  function onStarClicked() {
+    commitMutation({
+      variables: {
+        postId: data.id,
+        starred: !data.starred,
+      },
+    });
+  }
+
   return (
-    <div className="Star">
+    <div className="Star" onClick={onStarClicked}>
       {data.starred ? (
         <i className="fa-solid fa-star"></i>
       ) : (
