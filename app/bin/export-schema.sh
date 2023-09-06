b1928bcfa0766f908dc48140d7c6af76ffe055bb
Akshaya Acharya <akshaya.acharya.01@gmail.com>
2023-09-06 16:47:19 +0530

    Export hasura v3 schema and commands

--- app/bin/export-schema.sh
+++ app/bin/export-schema.sh
@@ -1,1 +1,5 @@
-npm exec -- gq http://hasura:8080/v1beta1/relay -H "X-Hasura-Admin-Secret: aFyrZOSGqAj2Eob5lNWz" --introspect > src/schema.graphql
+npm install -g get-graphql-schema
+get-graphql-schema https://ethical-doberman-9365.ddn.hasura.app/graphql -h "hasura_cloud_pat=2kzOoBVNwNAy2BJqOSlTiUFCiRZboBWpV6Q4RfCTEefDwmE815aeHRkeDhG5Zv6r" > src/schema.graphql
+
+# npm install -g graphqurl
+# npm exec -- gq http://hasura:8080/v1beta1/relay -H "X-Hasura-Admin-Secret: aFyrZOSGqAj2Eob5lNWz" --introspect > src/schema.graphql
